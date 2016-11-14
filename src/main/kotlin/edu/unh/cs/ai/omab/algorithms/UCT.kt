7f16d0b0e6301f14b670422eda022bc379cbd71b
katman <sammie.katt@gmail.com>
2016-11-13 21:16:55 -0500

    implemented most functions in uct

--- src/main/kotlin/edu/unh/cs/ai/omab/algorithms/UCT.kt
+++ src/main/kotlin/edu/unh/cs/ai/omab/algorithms/UCT.kt
@@ -17,8 +17,8 @@ import java.util.stream.IntStream
  * Assumes the Bandit problem with 2 arms and is meant to solve the problem
  * where the expected reward of each arm is unknown and represented as a belief state
  *
- * @param simulator is the generative simulator that is used to step through the tree
- * @param num_simulations is the amount of simulations to use while building the tree
+ * @param simulator is the generative simulator that is used to perform the steps 
+ * @param num_simulations is the amount of simulations used by UCT
  * @param horizon is the horizon of the problem
  */
 class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon: Int) {
@@ -26,12 +26,14 @@ class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon
     /**
      * @brief The current time step UCT is planning for
      */
-    private var curTime = 0
+    private var currentTimeStep = 0
 
     /**
-     * @brief A node in the UCT tree / graph
+     * @brief A node in the UCT datastructure
      *
      * Contains the q value of - and amount of times the action has been taken
+     *
+     * @TODO: generalize to N <q,n> pairs for N is the amount of possible actions.
      */
     data class UCTNode(var leftQ: Double, var leftN: Int, var rightQ: Double, var rightN: Int)
 
@@ -43,15 +45,70 @@ class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon
      */
     private var graph: MutableMap<BeliefState, UCTNode> = HashMap()
 
+    /**
+     * @brief Updates a node of the UCT datastructure given a taken action and perceived q value
+     *
+     * Increments the count associated with the action
+     * Calculates the new mean for the expected return of the associated action
+     *
+     * @TODO: make sure the provided node is a reference, not a copy
+     *
+     * @param node the node to update
+     * @param action taken action
+     * @param q the return from a single simulation from the node after taking action
+     *
+     * @return void
+     */
     private fun updateUCTNode(node: UCTNode, action: Action, q: Double) {
-        // @TODO: update q values of node (incremental average)
+        // @TODO: I'm sure kotlin has some nicer syntax than this...
         if (action == LEFT) {
             node.leftN++
+            node.leftQ += (q - node.leftQ) / leftN
         } else if (action == RIGHT) {
             node.rightN++
+            node.rightQ += (q - node.rightQ) / rightN
         } else {
             throw RuntimeException("UCT did not create any Q values associated with the rootState")
         }
+    }
+
+    /**
+    * @brief The UCT rollout is a simulation where the actions are taken randomly until the horizon has been reached
+    *
+    * Adds the starting point node to the UCT datastructure 
+    *
+    * @param state the starting state
+    * @param depth the starting depth from the search tree
+    *
+    * @return void
+    */
+    private fun rollout(state: BeliefState, depth: Int): Double {
+
+        // base case: reached horizon
+        if (reachedHorizon(depth)){
+            return 0
+        }
+
+        val FirstAction = if (Random() < 0.50) LEFT else RIGHT
+        val (nextState, rolloutReturn) = simulator.transition(state, action)
+
+        // @TODO: add check whether state is terminal to generlize to other problems
+        // rollout until horizon reached
+        while (reachedHorizon(++depth)) {
+            val action = if (Random() < 0.50) LEFT else RIGHT
+            val (nextState, reward) = simulator.transition(state, action)
+            rolloutReturn += reward;
+        }
+
+        // create and add new node to datastructure
+        newNode = if (FirstAction == LEFT) {
+            UCTNode(rolloutReturn, 1, 0, 0)
+        } else if (FirstAction == RIGHT) {
+            UCTNode(0, 0, rolloutReturn, 1)
+        } else {
+            throw RuntimeException("UCT did not create any Q values associated with the rootState")
+        }
+        graph.put(state, newNode)
     }
 
     /**
@@ -59,20 +116,21 @@ class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon
      *
      * Most importantly will stop when the horizon has been reached
      * Changes into random rollouts when a leave has been reached
-     * Adds a node to the tree at the start of each rollout
+     * Adds a node to the datastructure at the start of each rollout
      *
      * @param state the current state
-     * @param depth the current depth in the tree
+     * @param depth the current depth in the datastructure
      *
      * @return
      */
     private fun recurTreeSearch(state: BeliefState, depth: Int): Double {
 
-        // base case: reached end up horizon and return
-        if (depth + curTime >= horizon) {
+        // base case: return if horizon has been reached
+        if (reachedHorizon(depth)) {
             return 0.0
         }
 
+        // @TODO: make sure this is a reference (not a copy)
         var uctNode = graph[state]
 
         // perform random rollouts if reached outside of UCT explored tree
@@ -92,14 +150,14 @@ class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon
     }
 
     /**
-     * @brief Builds the UCT tree from root state assuming timestep
+     * @brief Builds the UCT datastructure from root state assuming timestep
      *
-     * @param rootState the root state of the tree
+     * @param rootState the root state of the datastructure
      *
      * @return void
      */
     private fun buildTree(rootState: BeliefState) {
-        assert(curTime < horizon)
+        assert(!reachedHorizon(0))
 
         // start empty
         graph.clear()
@@ -121,11 +179,10 @@ class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon
      * @return an action
      */
     fun selectAction(rootState: BeliefState, timestep: Int): Action {
-        curTime = timestep
+        currentTimeStep = timestep
 
         buildTree(rootState)
 
-        // @TODO: does this have to be so ugly?
         val rootQNode = graph[rootState] ?:
                 throw RuntimeException("UCT did not create any Q values associated with the rootState")
 
@@ -137,14 +194,16 @@ class UCTPlanner(val simulator: Simulator, val num_simulations: Int, val horizon
 /**
  * @brief Applies UCT on the provided MDP
  *
- * Currently will apply depth of the tree search up till horizon
+ * @TODO Currently will apply search all the way to the horizon, may make use of a max depth in future
+ * @TODO documentation on parameters
+ *
  * @param MDP
  * @param Int
  * @param Simulator
  *
- * @return return
+ * @return accumulated reward of a single instance of UCT planning on this problem 
  */
-fun uct(mdp: MDP, horizon: Int, simulator: Simulator): Long {
+fun uct(mdp: MDP, horizon: Int, world: Simulator, simulator: Simulator): Long {
 
     // @TODO: get some actual way of determine when to terminate UCT
     var num_simulations = 1000
@@ -158,9 +217,9 @@ fun uct(mdp: MDP, horizon: Int, simulator: Simulator): Long {
 
         // apply action
         val (nextState, reward) = if (action == LEFT) {
-            simulator.transition(currentState, LEFT)
+            world.transition(currentState, LEFT)
         } else if (action == RIGHT) {
-            simulator.transition(currentState, RIGHT)
+            world.transition(currentState, RIGHT)
         } else {
             throw RuntimeException("Impossible action selected")
         }
