ac77297a7e2418dff56ca93c1131adc1ca09baa5
ReazulHasan <reazul.hasan23@gmail.com>
2016-11-16 23:49:40 -0500

    modified RTDP

--- src/main/kotlin/edu/unh/cs/ai/omab/domain/MDP.kt
+++ src/main/kotlin/edu/unh/cs/ai/omab/domain/MDP.kt
@@ -9,7 +9,7 @@ import java.util.*
  */
 
 data class BeliefState(val alphaLeft: Int, val betaLeft: Int, val alphaRight: Int, val betaRight: Int) {
-    val utility = 0.0
+    var utility = 0.0
 
     fun leftSum() = alphaLeft + betaLeft
     fun rightSum() = alphaRight + betaRight
