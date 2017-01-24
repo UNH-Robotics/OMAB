d35bfd15479f6ba703360e5a6396bdbab64cd9a1
Bence Cserna <bence@cserna.net>
2017-01-24 14:53:24 -0500

    Format Python code

--- python_code/comparison.py
+++ python_code/comparison.py
@@ -1,11 +1,9 @@
-import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-#import seaborn
+import numpy as np
 from math import sqrt, log
 from random import random
 
-# Make sure we have no type 3 fonts
-import matplotlib
 matplotlib.rcParams['ps.useafm'] = True
 matplotlib.rcParams['pdf.use14corefonts'] = True
 matplotlib.rcParams['text.usetex'] = True
@@ -15,8 +13,11 @@ matplotlib.rcParams['text.usetex'] = True
 
 def bernoulli(p):
     """ Sample from Bernoulli distribution """
-    if random() <= p: return 1
-    else: return 0
+    if random() <= p:
+        return 1
+    else:
+        return 0
+
 
 ## Evaluation method
 
@@ -45,34 +46,38 @@ def evaluate(method, horizon, runs):
         up to that point. Note that even this is a single run, the rewards used in 
         computing the regret are the expected values and not the actual realizations
     """
-    
-    if type(runs) == int:
-        runs = (None,)*runs
-    
-    regrets = - np.ones( (len(runs), horizon) )
 
-    for irun, run in enumerate(runs):        
+    if type(runs) == int:
+        runs = (None,) * runs
+
+    regrets = - np.ones((len(runs), horizon))
+
+    for irun, run in enumerate(runs):
         # generate problem 
         if run is None:
-            pA = np.random.beta(1,1); pB = np.random.beta(1,1); 
+            pA = np.random.beta(1, 1);
+            pB = np.random.beta(1, 1);
         else:
-            pA,pB = run
+            pA, pB = run
         maxp = max(pA, pB)
         # initialize
-        losses = -np.ones(horizon); 
+        losses = -np.ones(horizon);
         m = method()
         # simulate
         for t in range(horizon):
-            arm = m.choose(t+1)
-            if arm == 0: p = bernoulli(pA)
-            else: p = bernoulli(pB)            
+            arm = m.choose(t + 1)
+            if arm == 0:
+                p = bernoulli(pA)
+            else:
+                p = bernoulli(pB)
             # update the algorithm
-            m.update(arm,p)            
+            m.update(arm, p)
             # update the regret (using the expected regret)
-            losses[t] = (maxp - (pA if arm==0 else pB))
-        regrets[irun,:] = np.cumsum(losses)     
+            losses[t] = (maxp - (pA if arm == 0 else pB))
+        regrets[irun, :] = np.cumsum(losses)
     return regrets
-    
+
+
 ## New Methods
 
 
@@ -81,58 +86,72 @@ class OptimisticLookAhead:
     Optimistic Look Ahead based on OGI paper by Gutin & Farias
     This is a trial implementation right now which is not necessarily correct
     """
+
     def __init__(self):
         # initialize prior values
-        self.Acountpos = 1; self.Acountneg = 1;
-        self.Bcountpos = 1; self.Bcountneg = 1;
+        self.Acountpos = 1;
+        self.Acountneg = 1;
+        self.Bcountpos = 1;
+        self.Bcountneg = 1;
         self.betasamplecount = 500
-    
+
     def choose(self, t):
         """ Which arm to choose; t is the current time step. Returns arm index """
-        
-        tRemain = horizon - ( (self.Acountpos + self.Acountneg + self.Bcountpos + self.Bcountneg) - 4 )
-        
+
+        tRemain = horizon - ((self.Acountpos + self.Acountneg + self.Bcountpos + self.Bcountneg) - 4)
+
         # TODO: WE NEED TO EXPLAIN THIS!!!
         discount = 0.95
-        tRemain = (1-discount**tRemain)/(1-discount)
+        tRemain = (1 - discount ** tRemain) / (1 - discount)
 
-        v01 = np.mean(np.max( np.row_stack( (np.random.beta(self.Acountpos+1, self.Acountneg,size=self.betasamplecount),
-                        np.random.beta(self.Bcountpos, self.Bcountneg ,size=self.betasamplecount) ) ),axis=0)) * tRemain
-                        
-        v02 = np.mean(np.max( np.row_stack( (np.random.beta(self.Acountpos, self.Acountneg+1,size=self.betasamplecount),
-                np.random.beta(self.Bcountpos, self.Bcountneg ,size=self.betasamplecount) )), axis=0)) * tRemain
+        v01 = np.mean(
+            np.max(np.row_stack((np.random.beta(self.Acountpos + 1, self.Acountneg, size=self.betasamplecount),
+                                 np.random.beta(self.Bcountpos, self.Bcountneg, size=self.betasamplecount))),
+                   axis=0)) * tRemain
 
-        v11 = np.mean(np.max( np.row_stack( (np.random.beta(self.Acountpos, self.Acountneg,size=self.betasamplecount),
-                        np.random.beta(self.Bcountpos+1, self.Bcountneg ,size=self.betasamplecount) ) ), axis=0)) * tRemain
-                        
-        v12 = np.mean(np.max( np.row_stack( (np.random.beta(self.Acountpos, self.Acountneg,size=self.betasamplecount),
-                        np.random.beta(self.Bcountpos, self.Bcountneg+1 ,size=self.betasamplecount) ) ), axis=0 )) * tRemain
-        
-        pA = self.Acountpos/(self.Acountpos+self.Acountneg)
-        valueA = pA*(1+v01) + (1 - pA)*v02
-        
-        pB = self.Bcountpos/(self.Bcountpos+self.Bcountneg)
-        valueB = pB*(1+v11) + (1-pB)*v12
+        v02 = np.mean(
+            np.max(np.row_stack((np.random.beta(self.Acountpos, self.Acountneg + 1, size=self.betasamplecount),
+                                 np.random.beta(self.Bcountpos, self.Bcountneg, size=self.betasamplecount))),
+                   axis=0)) * tRemain
 
-        #print(v01,v02,v11,v12)
-        #print(pA, pB)
-        #print(self.Acountpos, self.Acountneg, valueA, self.Bcountpos, self.Bcountneg, valueB)
-        #print('-----')
-        
-        if valueA > valueB: return 0
-        else: return 1
-        
-    def update(self,arm, outcome):
+        v11 = np.mean(np.max(np.row_stack((np.random.beta(self.Acountpos, self.Acountneg, size=self.betasamplecount),
+                                           np.random.beta(self.Bcountpos + 1, self.Bcountneg,
+                                                          size=self.betasamplecount))), axis=0)) * tRemain
+
+        v12 = np.mean(np.max(np.row_stack((np.random.beta(self.Acountpos, self.Acountneg, size=self.betasamplecount),
+                                           np.random.beta(self.Bcountpos, self.Bcountneg + 1,
+                                                          size=self.betasamplecount))), axis=0)) * tRemain
+
+        pA = self.Acountpos / (self.Acountpos + self.Acountneg)
+        valueA = pA * (1 + v01) + (1 - pA) * v02
+
+        pB = self.Bcountpos / (self.Bcountpos + self.Bcountneg)
+        valueB = pB * (1 + v11) + (1 - pB) * v12
+
+        # print(v01,v02,v11,v12)
+        # print(pA, pB)
+        # print(self.Acountpos, self.Acountneg, valueA, self.Bcountpos, self.Bcountneg, valueB)
+        # print('-----')
+
+        if valueA > valueB:
+            return 0
+        else:
+            return 1
+
+    def update(self, arm, outcome):
         """ Updates the estimate for the arm outcome """
         if arm == 0:
-            if outcome == 1: self.Acountpos += 1
-            else: self.Acountneg += 1
+            if outcome == 1:
+                self.Acountpos += 1
+            else:
+                self.Acountneg += 1
         elif arm == 1:
-            if outcome == 1: self.Bcountpos += 1
-            else: self.Bcountneg += 1
+            if outcome == 1:
+                self.Bcountpos += 1
+            else:
+                self.Bcountneg += 1
         else:
             raise RuntimeError("Invalid arm number")
-    
 
 
 horizon = 100
@@ -146,9 +165,9 @@ plt.xlabel('Time step')
 plt.ylabel('Regret')
 plt.grid()
 plt.savefig('regrets.pdf')
-plt.show()            
-            
-            
+plt.show()
+
+
 ## Simple methods            
 
 class UCB:
@@ -157,21 +176,27 @@ class UCB:
     just to make the method independent of the order of arms and the sensitivity
     with respect to the initialization.
     """
-    def __init__(self,alpha = 2.0):
-        self.Amean = 0; self.Bmean = 0; 
-        self.Acount = 1; self.Bcount = 1;
+
+    def __init__(self, alpha=2.0):
+        self.Amean = 0
+        self.Bmean = 0
+        self.Acount = 1
+        self.Bcount = 1
         self.alpha = alpha
-        
+
     def choose(self, t):
         """ Which arm to choose; t is the current time step. Returns arm index """
-        valA = self.Amean + sqrt((self.alpha * log(t)) / (2*self.Acount) )
-        valB = self.Bmean + sqrt((self.alpha * log(t)) / (2*self.Bcount) )
+        valA = self.Amean + sqrt((self.alpha * log(t)) / (2 * self.Acount))
+        valB = self.Bmean + sqrt((self.alpha * log(t)) / (2 * self.Bcount))
         # the UCB choice
-        if valA > valB: return 0
-        elif valA < valB: return 1
-        else: return bernoulli(0.5)
-    
-    def update(self,arm, outcome):
+        if valA > valB:
+            return 0
+        elif valA < valB:
+            return 1
+        else:
+            return bernoulli(0.5)
+
+    def update(self, arm, outcome):
         """ Updates the estimate for the arm outcome """
         if arm == 0:
             self.Amean = (self.Amean * self.Acount + outcome) / (self.Acount + 1)
@@ -187,38 +212,49 @@ class Thompson:
     """
     Thompson sampling
     """
+
     def __init__(self):
         # initialize prior values
-        self.Acountpos = 1; self.Acountneg = 1;
-        self.Bcountpos = 1; self.Bcountneg = 1;
-    
+        self.Acountpos = 1
+        self.Acountneg = 1
+        self.Bcountpos = 1
+        self.Bcountneg = 1
+
     def choose(self, t):
         """ Which arm to choose; t is the current time step. Returns arm index """
         pA = np.random.beta(self.Acountpos, self.Acountneg)
         pB = np.random.beta(self.Bcountpos, self.Bcountneg)
-        if pA > pB: return 0
-        else: return 1
-        
-    def update(self,arm, outcome):
+        if pA > pB:
+            return 0
+        else:
+            return 1
+
+    def update(self, arm, outcome):
         """ Updates the estimate for the arm outcome """
         if arm == 0:
-            if outcome == 1: self.Acountpos += 1
-            else: self.Acountneg += 1
+            if outcome == 1:
+                self.Acountpos += 1
+            else:
+                self.Acountneg += 1
         elif arm == 1:
-            if outcome == 1: self.Bcountpos += 1
-            else: self.Bcountneg += 1
+            if outcome == 1:
+                self.Bcountpos += 1
+            else:
+                self.Bcountneg += 1
         else:
             raise RuntimeError("Invalid arm number")
-            
-            
+
+
 ## Gittins index
 
 # loads the index as a global variable (to avoid reinit in every run)
 gittins = {}
 import pandas as pa
+
 gittins_csv = pa.read_csv('gittins.csv')
-for (p,n,v) in zip(gittins_csv.Positive, gittins_csv.Negative, gittins_csv.Index):
-    gittins[(p,n)] = v
+for (p, n, v) in zip(gittins_csv.Positive, gittins_csv.Negative, gittins_csv.Index):
+    gittins[(p, n)] = v
+
 
 class Gittins:
     """
@@ -226,30 +262,41 @@ class Gittins:
     just to make the method independent of the order of arms and the sensitivity
     with respect to the initialization.
     """
+
     def __init__(self):
         # initialize prior values
-        self.Acountpos = 1; self.Acountneg = 1;
-        self.Bcountpos = 1; self.Bcountneg = 1;
-    
+        self.Acountpos = 1
+        self.Acountneg = 1
+        self.Bcountpos = 1
+        self.Bcountneg = 1
+
     def choose(self, t):
         """ Which arm to choose; t is the current time step. Returns arm index """
         pA = gittins[(self.Acountpos, self.Acountneg)]
         pB = gittins[(self.Bcountpos, self.Bcountneg)]
-        if pA > pB: return 0
-        elif pA < pB: return 1
-        else: return bernoulli(0.5)
-        
-    def update(self,arm, outcome):
+        if pA > pB:
+            return 0
+        elif pA < pB:
+            return 1
+        else:
+            return bernoulli(0.5)
+
+    def update(self, arm, outcome):
         """ Updates the estimate for the arm outcome """
         if arm == 0:
-            if outcome == 1: self.Acountpos += 1
-            else: self.Acountneg += 1
+            if outcome == 1:
+                self.Acountpos += 1
+            else:
+                self.Acountneg += 1
         elif arm == 1:
-            if outcome == 1: self.Bcountpos += 1
-            else: self.Bcountneg += 1
+            if outcome == 1:
+                self.Bcountpos += 1
+            else:
+                self.Bcountneg += 1
         else:
             raise RuntimeError("Invalid arm number")
-    
+
+
 ## Compute the mean regret
 
 horizon = 100
@@ -259,7 +306,7 @@ ucb_regrets = evaluate(UCB, horizon, trials)
 thompson_regrets = evaluate(Thompson, horizon, trials)
 ola_regrets = evaluate(OptimisticLookAhead, horizon, trials)
 gittins_regrets = evaluate(Gittins, horizon, trials)
-    
+
 ## Plot the mean regret
 
 plt.plot(ucb_regrets.mean(0), label='UCB')
@@ -279,8 +326,10 @@ ticks = 30
 repetitions = 50
 
 # init p values
-runs = tuple((p1, p2) for p1 in np.linspace(0,1,ticks) for p2 in np.linspace(0,1,ticks) for r in range(repetitions) if p1 != p2) 
-deltas = np.array(tuple(abs(pA-pB) for pA, pB in runs))
+runs = tuple(
+    (p1, p2) for p1 in np.linspace(0, 1, ticks) for p2 in np.linspace(0, 1, ticks) for r in range(repetitions) if
+    p1 != p2)
+deltas = np.array(tuple(abs(pA - pB) for pA, pB in runs))
 
 ucb_regrets = evaluate(UCB, horizon, runs)
 thompson_regrets = evaluate(Thompson, horizon, runs)
@@ -290,20 +339,25 @@ gittins_regrets = evaluate(Gittins, horizon, runs)
 ## Plot dependence on delta
 
 # average the 
-shrunkdelta = deltas.reshape(-1, repetitions)[:,0]
-shrunkprobs = np.array([max(p1,p2) for p1,p2 in runs]).reshape(-1, repetitions)[:,0]
+shrunkdelta = deltas.reshape(-1, repetitions)[:, 0]
+shrunkprobs = np.array([max(p1, p2) for p1, p2 in runs]).reshape(-1, repetitions)[:, 0]
+
 
 def plot_curve(data, pos, name):
-    plt.subplot(1,4,pos)
-    plt.scatter(shrunkdelta,data[:,-1].reshape(-1,repetitions).mean(1)/(shrunkprobs*horizon)*100, s=10, c=shrunkprobs, edgecolors='face',cmap=matplotlib.cm.plasma)
-    plt.ylim(-1,30)
-    plt.xlabel('$\Delta$'); plt.ylabel('Mean propotional regret (\%)'); plt.title(name)
+    plt.subplot(1, 4, pos)
+    plt.scatter(shrunkdelta, data[:, -1].reshape(-1, repetitions).mean(1) / (shrunkprobs * horizon) * 100, s=10,
+                c=shrunkprobs, edgecolors='face', cmap=matplotlib.cm.plasma)
+    plt.ylim(-1, 30)
+    plt.xlabel('$\Delta$')
+    plt.ylabel('Mean propotional regret (\%)')
+    plt.title(name)
     plt.grid()
+
 
 plt.figure(num=None, figsize=(10, 6), dpi=80, facecolor='w', edgecolor='k')
 plot_curve(ucb_regrets, 1, "UCB")
-plot_curve(thompson_regrets,2, "Thompson")
-plot_curve(ola_regrets,3, "OptimisticLookahed")
+plot_curve(thompson_regrets, 2, "Thompson")
+plot_curve(ola_regrets, 3, "OptimisticLookahed")
 plot_curve(gittins_regrets, 4, "Gittins")
 
 plt.savefig('proportional_regret.pdf')
