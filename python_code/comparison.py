d1b6361d3b97b6f11113a3185a4b07095dd33fd7
Bence Cserna <bence@cserna.net>
2017-02-06 23:29:16 -0500

    Format Python code

--- python_code/comparison.py
+++ python_code/comparison.py
@@ -57,13 +57,13 @@ def evaluate(method, horizon, runs):
     for irun, run in enumerate(runs):
         # generate problem 
         if run is None:
-            pA = np.random.beta(1, 1);
-            pB = np.random.beta(1, 1);
+            pA = np.random.beta(1, 1)
+            pB = np.random.beta(1, 1)
         else:
             pA, pB = run
         maxp = max(pA, pB)
         # initialize
-        losses = -np.ones(horizon);
+        losses = -np.ones(horizon)
         m = method()
         # simulate
         for t in range(horizon):
@@ -91,10 +91,10 @@ class OptimisticLookAhead:
 
     def __init__(self):
         # initialize prior values
-        self.Acountpos = 1;
-        self.Acountneg = 1;
-        self.Bcountpos = 1;
-        self.Bcountneg = 1;
+        self.Acountpos = 1
+        self.Acountneg = 1
+        self.Bcountpos = 1
+        self.Bcountneg = 1
         self.betasamplecount = 500
 
     def choose(self, t):
@@ -298,6 +298,7 @@ class Gittins:
         else:
             raise RuntimeError("Invalid arm number")
 
+
 ## Compute the mean regret
 
 horizon = 100
@@ -308,13 +309,15 @@ thompson_regrets = evaluate(Thompson, horizon, trials)
 ola_regrets = evaluate(OptimisticLookAhead, horizon, trials)
 gittins_regrets = evaluate(Gittins, horizon, trials)
 
+
 ## Confidence interval
 def calc_confidence_interval(data, confidence=0.95):
-    a = 1.0*np.array(data)
+    a = 1.0 * np.array(data)
     n = len(a)
     m, se = np.mean(a), scipy.stats.sem(a)
-    e = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+    e = se * sp.stats.t._ppf((1 + confidence) / 2., n - 1)
     return m, e
+
 
 ymean = []
 yconf = []
@@ -383,7 +386,7 @@ def plot_curve(data, pos, name):
 plt.figure(num=None, figsize=(10, 6), dpi=80, facecolor='w', edgecolor='k')
 plot_curve(ucb_regrets, 1, "UCB")
 plot_curve(thompson_regrets, 2, "Thompson")
-plot_curve(ola_regrets, 3, "OptimisticLookahed")
+plot_curve(ola_regrets, 3, "OptimisticLookahead")
 plot_curve(gittins_regrets, 4, "Gittins")
 
 plt.savefig('proportional_regret.pdf')
